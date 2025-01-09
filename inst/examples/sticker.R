aad313ba40d4542c2b9add6c0695802c53812183
Marc Burri <marc.burri91@gmail.com>
2025-01-09 17:02:19 +0100

    update sticker

--- inst/examples/sticker.R
+++ inst/examples/sticker.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(hexSticker)
 library(reviser)
+library(dplyr)
 
 file_name <- "logo.png"
 file_path <- "man/figures/"
@@ -9,7 +10,7 @@ file_path
 
 
   
- ( plot_vintages(gdp_long_pc %>% filter(pub_date > as.Date("2009-01-01") & pub_date < as.Date("2012-10-01")), type = "line") +
+ ( reviser::plot_vintages(gdp_long_pc %>% filter(pub_date > as.Date("2009-01-01") & pub_date < as.Date("2012-10-01")), type = "line") +
   xlim(as.Date("2008-01-01"), as.Date("2010-07-01")) +# + ylim(12500, 14500)
   theme_void() +
   theme_transparent() +
@@ -19,17 +20,20 @@ file_path
     # Font size for package name.
     p_size = 20,
     #p_color = "cornflowerblue",
+    p_family = "mono",
     s_x = 1,
     s_y = 1,
-    s_width = 1.5,
-    s_height = 1,
+    s_width = 1.9,
+    s_height = 1.3,
     # y position for package name.
     p_y = 1.5,
     # Color to fill hexagon.
-    h_fill = "steelblue4",
+    h_fill = "steelblue1",
     # Color for hexagon border.
-    h_color	= "cornflowerblue",
+    #h_color	= "cornflowerblue",
+    h_color	= "steelblue",
     filename = file_path,
-    url = ""
+    url = "github.com/p-wegmueller/ReviseR",
+    u_size = 4
   )
 
